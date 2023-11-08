ccd1187e3d2943659ce0d2762e01a7eef6bc2425
Sanath Swaroop <sanath15swaroop@gmail.com>
2023-11-07 22:22:46 -0500

    added comments

--- package.py
+++ package.py
@@ -19,17 +19,17 @@ def breakdown_qr():
     Breaks down the QR code image into smaller pieces and saves each piece as separate image files.
     """
     original_image = Image.open("qr.jpg")
-
+    # Get the width and height of the image from the size method
     width, height = original_image.size
-
+    # Define the number of rows and coloumns the qr needs to be split into
     rows = 25
     columns = 25
-
+    # Determine the width and the height of each piece
     piece_width = width // columns
     piece_height = height // rows
-
+    # Define the array for image pieces to be stored in
     image_pieces = []
-
+    # Iterate through the loop and crop the image and crop the images based on the individual coordinates determined. store the image in the format piece_0_5.jpg, append the image_pieces array
     for i in range(rows):
         for j in range(columns):
             left = j * piece_width
@@ -44,22 +44,28 @@ def add_border_to_image(path):
     """
     Creates an image with a border of smaller pieces around a central image, using the QR code pieces.
     """
+    # load the central image that was selected by the user. 
     central_image = Image.open(path)
+    # load the number of rows and coloumns that the qr was cut into
     rows = 25
     columns = 25
     border_images = []
+    # load the border images from the file system that were stored earlier.
     for i in range(rows):
         for j in range(columns):
             piece = Image.open(f"piece_{i}_{j}.jpg")
             border_images.append(piece)
+    # Determine the updated width and height of the final image after adding the border images as the border.
     width = central_image.width + 2 * max(image.width for image in border_images)
     height = central_image.height + int(2 * 35/25 *  max(image.height for image in border_images))
+    # get the height of the qr border
     qr_height = int(height - central_image.height) 
+    # create a new canvas with the extra width and height
     final_image = Image.new("RGB", (width, height))
     x_border = 0
     y_border = 0
     border_image = Image.open(f"piece_0_0.jpg")
-    
+    # Logic for appending the broder images to the border as a frame. 
     for k in range(1):
         for i in range(25):
             for j in range(25):
@@ -70,11 +76,13 @@ def add_border_to_image(path):
                     y_border+= border_image.height
                     x_border = 0
                 k+=1
+    # Attach the border to the bottom of the image
     x_central = (width - central_image.width) // 2
     y_central = y_border + border_image.height
     final_image.paste(central_image, (x_central, y_central))
     Counter = 100
     bkp_y_border = y_border
+    # fill the left and right with some random border images
     while(x_border<final_image.width and y_border<final_image.height):
         if(x_border < x_central):
             final_image.paste(border_images[Counter], (x_border,y_border))
@@ -100,19 +108,20 @@ def add_border_to_image(path):
         else:
             x_border = int(central_image.width + (final_image.width - central_image.width)/2)
             y_border += border_image.height
+    # save the final image
     final_image.save("final_image_before_border.jpg")
 
     qr_distorted_image = final_image.crop((0, 0, final_image.width, qr_height*1.5))
+    # replicate the top border to the bottom
     final_image.paste(qr_distorted_image, (0, final_image.height-int(qr_height*1.5)))
-
+    # load the image and read meta data
     final_image.save("final_image.jpg")
     exif_dict = piexif.load("final_image.jpg")
+    # add the border image height as metadata
     userdata = {
-        'dim': border_image.height,
-        'author' : "Sanath Swaroop Mulky",
-        'webpage' : "https://sanathswaroop.com",
-        'project_url' : "https://github.com/sanath1599/CipherQR"
+        'dim': border_image.height
     }
+    # store the meta data
     exif_dict["Exif"][piexif.ExifIFD.UserComment] = piexif.helper.UserComment.dump(
         json.dumps(userdata),
         encoding="unicode"
@@ -126,10 +135,13 @@ def cut_border_from_image(path):
     """
     Cuts the border from the final image and saves the reconstructed QR code.
     """
+    # Load the image to cut the border from based on user input
     main_image = Image.open(path)
+    # extract metadata
     exif_dict = piexif.load(path)
     user_comment = piexif.helper.UserComment.load(exif_dict["Exif"][piexif.ExifIFD.UserComment])
     d = json.loads(user_comment)
+    # load the border image size from the metadata
     piece_size = d['dim']
     final_image = Image.new("RGB", (1000, 1000))
     x = 0
@@ -138,6 +150,7 @@ def cut_border_from_image(path):
     Counter = 0
     row = 0
     col = 0
+    # while the Counter is less than the total number of pieces, cut the border of each individual image and add it to the final image(reconstructed qr)
     while(Counter < 625):
         piece = main_image.crop((col * piece_size, row * piece_size, (col + 1) * piece_size, (row + 1) * piece_size))
         col +=1
@@ -153,6 +166,7 @@ def cut_border_from_image(path):
     final_image = final_image.convert('L')
     width, height = final_image.size
     left, right, top, bottom = width, 0, height, 0
+    # check for the last black pixel and add 15 inches to it and crop it
     for x in range(width):
         for y in range(height):
             pixel = final_image.getpixel((x, y))
@@ -164,10 +178,3 @@ def cut_border_from_image(path):
     final_image = final_image.crop((left - 15, top - 15, right + 15, bottom + 15))
 
     final_image.save("reconstructed_qr.jpg")
-
-# generate_qr("THIS\n\n\n\n\gnjdsfngjlkdsnfgm.,xcnzv,mcnxfjn;rawefjhljkdn,czmxnkgfjdskrjg'pjra'gijf;lkdgjfkl;djgk;fldjg;fkldsjgfkldjgfkldsjgkfl;sdgjfkl;dsjgfk;ldjgfkldjgfk;ldjgkfldjgfkdljgflkjhfkhjg[iowaj0iepjsdf,mnvxj!@#$%^&*())(*&^%$#@!@#$%^&*()(*&^%$#@!@#$%^&*()(*&^%$#@!@#$%^&*()*&^%$#)))] IS THE DATEjrghusdhvjksdhfbuachfausdhfuihfguirhfgurhfljdshfjxcnzv,jxcn")
-# generate_qr("THIS IS THE DATATHIS IS THE DATATHIS IS THE DATATHE DATATHIS IS THE DATATHE DATATHIS IS THE DATATHE DATATHIS IS THE DATATHE DATATHIS IS THE DATATHE DATATHIS IS THE DATATHIS IS THE DATATHIS IS THE DATATHIS IS THE DATATHIS IS THE DATATHIS IS THE DATA")
-# generate_qr("https://sanathswaroop.com")
-# breakdown_qr()
-# # add_border_to_image()
-# cut_border_from_image()
